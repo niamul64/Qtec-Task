dd6192ae9da4ef9f1ac3bb623fec59e6cd53534f
Niamul Hasan <48917799+niamul64@users.noreply.github.com>
2022-02-25 17:29:25 +0600

    check point

--- QtecTask/keywords/views.py
+++ QtecTask/keywords/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
+
+from .models import SearchDate, KewWordsMD
+
+
+def showKeyWords(request):
+
+    words = KewWordsMD.objects.all()
+    dates = SearchDate.objects.all()
+    dict = {'words': words, 'dates': dates}
+    return render(request, 'keyword/show.html', context=dict)
